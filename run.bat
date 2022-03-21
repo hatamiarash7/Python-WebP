7ce8b833cbabfae4eadad459e54fe1b137ee3447
Arash Hatami <hatamiarash7@gmail.com>
2022-03-21 14:30:54 +0330

    Make script looks better

--- run.bat
+++ run.bat
@@ -1,1 +1,23 @@
+@echo off
+
+Set /a num=(%Random% %%9)+1
+
+color %num%
+
+echo Start converting ...
+echo.
+echo      .-.
+echo     /   \         .-.
+echo    /     \       /   \       .-.     .-.     _   _
+echo --/-------\-----/-----\-----/---\---/---\---/-\-/-\/\/---
+echo  /         \   /       \   /     '-'     '-'
+echo /           '-'         '-'
+echo.
+
 py convert.py
+
+echo.
+echo Done ! Have fun
+echo.
+
+pause
